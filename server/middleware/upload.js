cf857e772b6e5627b0b9e52820c17f96ee1de267
okechukwu promise <okechukwupromise@gmail.com>
2026-05-18 09:55:49 +0100

    update

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -2,13 +2,15 @@ const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
 
-if (!fs.existsSync("uploads")) {
-  fs.mkdirSync("uploads");
+const uploadPath = path.join(__dirname, "../uploads");
+
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
 }
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/");
+    cb(null, uploadPath);
   },
 
   filename: function (req, file, cb) {
